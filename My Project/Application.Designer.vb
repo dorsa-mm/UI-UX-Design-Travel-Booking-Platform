8ecd0a44fb364f46d1bd1250d86fb7b92da41549
barbara <barbara.goss@torontomu.ca>
2024-11-08 15:37:03 -0500

    Added main form and changed reference to the start form.

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -31,7 +31,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = MainInterface
+            Me.MainForm = TimeBusForm
         End Sub
     End Class
 End Namespace
